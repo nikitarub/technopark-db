cf2f51186a14c336c4df23b39369a04c9008b8de
max <e@mai.ru>
2019-01-04 13:49:55 +0300

    development config

---
+++ forum/webpack.dev.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const CleanWebPackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    mode : 'development',
+
+    watch : true,
+    watchOptions : {
+        ignored : /node_modules/
+    },
+
+    entry : {
+        main : './server/index.js'
+    },
+
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+    },
+    
+    devtool : 'inline-source-map',
+
+    target : 'node',
+    externals : [nodeExternals()],
+
+    module : {
+        rules : [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ] 
+    },
+
+    plugins : [
+        new CleanWebPackPlugin(['dist'])
+    ]
+}
